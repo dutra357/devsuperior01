ecee5d03db51de89e8447a6cb0fe59e44744f33b
David Alves Dutra <dadutra@hotmail.com>
2024-11-16 08:13:18 -0300

    first commit

---
+++ src/main/java/com/exercicio/devsuperior01/Devsuperior01Application.java
@@ -0,0 +1,13 @@
+package com.exercicio.devsuperior01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devsuperior01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devsuperior01Application.class, args);
+	}
+
+}
